e79e83fbc6d6ec0b6be71a177f3f69a44ed1f0a6
Александр Кравченко <svhelpp@gmail.com>
2020-09-19 10:49:50 +0700

    - init;

---
+++ BusinessLogic/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BusinessLogic
+{
+    public class Class1
+    {
+        public void Init()
+        {
+            var list = new List<string>();
+        }
+    }
+}
